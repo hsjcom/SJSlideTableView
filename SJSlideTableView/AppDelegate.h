9624c7b75a4a50fd97beb485cd35076493d452ea
hsjcom <soldier@SoldierdeMacBook-Pro.local>
2015-04-23 11:19:22 +0800

    Initial Commit

---
+++ SJSlideTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SJSlideTableView
+//
+//  Created by Soldier on 15/4/23.
+//  Copyright (c) 2015年 Soldier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SJSlideTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SJSlideTableView
+//
+//  Created by Soldier on 15/4/23.
+//  Copyright (c) 2015年 Soldier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
